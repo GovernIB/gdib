8c36413438e5c598d5fde933d30e6300ea35f305
dsariego-memorandum <david.sariego@memorandum.net>
2020-10-20 11:55:45 +0200

    #9 Error quan hi ha caràcters especials dins nom de l'interessat Se utiliza una librería para escapar los caracteres especiales en el cliente api rest antes de que se envíen

--- gdib-apirest/src/main/java/es/caib/arxiudigital/apirest/facade/pojos/CabeceraPeticion.java
+++ gdib-apirest/src/main/java/es/caib/arxiudigital/apirest/facade/pojos/CabeceraPeticion.java
@@ -1,5 +1,7 @@
 package es.caib.arxiudigital.apirest.facade.pojos;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public class CabeceraPeticion {
 	private String serviceVersion;
 	private String documentoSolicitante;
@@ -18,67 +20,67 @@ public class CabeceraPeticion {
 		return serviceVersion;
 	}
 	public void setServiceVersion(String serviceVersion) {
-		this.serviceVersion = serviceVersion;
+		this.serviceVersion = StringEscapeUtils.escapeXml(serviceVersion);
 	}
 	public String getDocumentoSolicitante() {
 		return documentoSolicitante;
 	}
 	public void setDocumentoSolicitante(String documentoSolicitante) {
-		this.documentoSolicitante = documentoSolicitante;
+		this.documentoSolicitante = StringEscapeUtils.escapeXml(documentoSolicitante);
 	}
 	public String getNombreSolicitante() {
 		return nombreSolicitante;
 	}
 	public void setNombreSolicitante(String nombreSolicitante) {
-		this.nombreSolicitante = nombreSolicitante;
+		this.nombreSolicitante = StringEscapeUtils.escapeXml(nombreSolicitante);
 	}
 	public String getIdExpediente() {
 		return idExpediente;
 	}
 	public void setIdExpediente(String idExpediente) {
-		this.idExpediente = idExpediente;
+		this.idExpediente = StringEscapeUtils.escapeXml(idExpediente);
 	}
 	public String getNombreProcedimiento() {
 		return nombreProcedimiento;
 	}
 	public void setNombreProcedimiento(String nombreProcedimiento) {
-		this.nombreProcedimiento = nombreProcedimiento;
+		this.nombreProcedimiento = StringEscapeUtils.escapeXml(nombreProcedimiento);
 	}
 	public String getCodiAplicacion() {
 		return codiAplicacion;
 	}
 	public void setCodiAplicacion(String codiAplicacion) {
-		this.codiAplicacion = codiAplicacion;
+		this.codiAplicacion = StringEscapeUtils.escapeXml(codiAplicacion);
 	}
 	public String getUsuarioSeguridad() {
 		return usuarioSeguridad;
 	}
 	public void setUsuarioSeguridad(String usuarioSeguridad) {
-		this.usuarioSeguridad = usuarioSeguridad;
+		this.usuarioSeguridad = StringEscapeUtils.escapeXml(usuarioSeguridad);
 	}
 	public String getPasswordSeguridad() {
 		return passwordSeguridad;
 	}
 	public void setPasswordSeguridad(String passwordSeguridad) {
-		this.passwordSeguridad = passwordSeguridad;
+		this.passwordSeguridad = StringEscapeUtils.escapeXml(passwordSeguridad);
 	}
 	public String getNombreUsuario() {
 		return nombreUsuario;
 	}
 	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
+		this.nombreUsuario = StringEscapeUtils.escapeXml(nombreUsuario);
 	}
 	public String getDocumentoUsuario() {
 		return documentoUsuario;
 	}
 	public void setDocumentoUsuario(String documentoUsuario) {
-		this.documentoUsuario = documentoUsuario;
+		this.documentoUsuario = StringEscapeUtils.escapeXml(documentoUsuario);
 	}
 	public String getOrganizacion() {
 		return organizacion;
 	}
 	public void setOrganizacion(String organizacion) {
-		this.organizacion = organizacion;
+		this.organizacion = StringEscapeUtils.escapeXml(organizacion);
 	}
 
 
